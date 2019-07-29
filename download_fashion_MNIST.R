3ae504d98da67976247f1b367218f84545d82db0
DigitalDieter <devops@backupmailer.de>
2019-07-29 02:56:18 +0200

    download_fashion_MNIST.R added

---
+++ download_fashion_MNIST.R
@@ -0,0 +1,8 @@
+download.file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz",
+              "fashion-train-images-idx3-ubyte.gz")
+download.file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz",
+              "fashion-train-labels-idx1-ubyte.gz")
+download.file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz",
+              "fashion-t10k-images-idx3-ubyte.gz")
+download.file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz",
+              "fashion-t10k-labels-idx1-ubyte.gz")
