d7dc825f5164ece1bf6174763347ef829700cfdc
DigitalDieter <devops@backupmailer.de>
2019-07-29 02:58:07 +0200

    extract_fashion_MNIST.R added

---
+++ extract_fashion_MNIST.R
@@ -0,0 +1,5 @@
+# gunzip the files
+R.utils::gunzip("fashion-train-images-idx3-ubyte.gz")
+R.utils::gunzip("fashion-train-labels-idx1-ubyte.gz")
+R.utils::gunzip("fashion-t10k-images-idx3-ubyte.gz")
+R.utils::gunzip("fashion-t10k-labels-idx1-ubyte.gz")
