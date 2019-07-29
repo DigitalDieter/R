eda43b7eca270c527b0ccb7b744c10ba2e8eec35
DigitalDieter <devops@backupmailer.de>
2019-07-29 02:53:02 +0200

    extract_MNIST.R added

---
+++ extract_MNIST.R
@@ -0,0 +1,5 @@
+# gunzip the files
+R.utils::gunzip("train-images-idx3-ubyte.gz")
+R.utils::gunzip("train-labels-idx1-ubyte.gz")
+R.utils::gunzip("t10k-images-idx3-ubyte.gz")
+R.utils::gunzip("t10k-labels-idx1-ubyte.gz")
